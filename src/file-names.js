1c6dc237f5d4f38d751410aeccec5a5f49e72a4c
Marharyta Parkalava <margobraun777@gmail.com>
2023-12-18 02:21:03 +0300

    fix: done getEmailDomain

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const logs = {};
+  const devolt = [];
+  
+  for ( let i = 0; i < names.length; i++) {
+    let file = names[i];
+    let addition = '';
+
+    while (logs[file + addition]) {
+      addition = `(${logs[file + addition]++})`
+    }
+    logs[file + addition] = 1;
+    devolt.push(file + addition)
+  }
+  return devolt;
 }
 
 module.exports = {
