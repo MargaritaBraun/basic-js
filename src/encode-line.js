0dfbe5bbb2d924b7ddb0dccdaa838639f0594766
Marharyta Parkalava <margobraun777@gmail.com>
2023-12-17 23:18:09 +0300

    fix: done encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str.length === 0) {
+    return '';
+  }
+
+  let resurs = '';
+  let count = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      count++;
+    } else {
+      if (count > 1) {
+        resurs += count + str[i - 1];
+      } else {
+        resurs += str[i - 1];
+      }
+      count = 1;
+    }
+  }
+
+  if (count > 1) {
+    resurs += count + str[str.length - 1];
+  } else {
+    resurs += str[str.length - 1];
+  }
+
+  console.log(resurs);
+  return resurs;
 }
 
 module.exports = {
