0c370c2aafc52f2fd1e702e1917e081106532ede
Marharyta Parkalava <margobraun777@gmail.com>
2023-12-18 01:52:08 +0300

    fix: done repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (typeof str !== 'string') {
+    str = String(str);
+  }
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  let revolt = '';
+
+  //if (typeof repeatTimes === 'undefined') {
+    //repeatTimes = 1
+    //additionRepeatTimes = 1
+    //revolt = str + addition
+  //}
+
+  for (let i = 0; i < repeatTimes; i++) {
+    revolt += str;
+
+    if (additionRepeatTimes > 0) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        revolt += addition;
+
+        if (j < additionRepeatTimes - 1) {
+          revolt += additionSeparator;
+        }
+      }
+    }
+
+    if (i < repeatTimes - 1) {
+      //if (typeof separator === 'undefined') {
+        //separator = '+'
+      //}
+      revolt += separator;
+    }
+  }
+
+  return revolt;
 }
 
 module.exports = {
